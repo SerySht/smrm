d1d8c9e48ad2d224acc0ea5c98db9fce47657358
Sergey <sery.sht@gmail.com>
2017-03-22 01:49:55 +0300

    begin realize functions

--- MainTask/lab2.py
+++ MainTask/lab2.py
@@ -18,31 +18,39 @@ def delete_file(filename):
 def delete_directory():
 	shutil.rmtree(directory_location)
 
-def recover_trash():
-	pass
+def recover_from_trash(filename):
+	f = open('Trash/list.txt', 'r')
+	for line in f:
+		line = line.split()   #could be better way	
+		if filename == line[0]:			
+			shutil.move("Trash/"+filename, line[1])
+	f.close()
+	#add deleting from list
+
+
 
 def delete_in_trash(filename, file_location, trash_location):
 	shutil.move(filename, trash_location)
 	f = open('Trash/list.txt', 'a')
-	f.write(file_location+filename)
+	f.write(filename + ' ' + file_location)
 	f.close()
 
 
 
 
 
-
 def main():
-	#parser = argparse.ArgumentParser()
-	#parser.add_argument('--file', default = "filetest")
-	#parser.add_argument('--trash_location', default = 'testtrash')
-	#parser.add_argument('command')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('type_of_command')
+	parser.add_argument('file')	
+	file_location = os.getcwd()
+	trash_location = ("Trash")
 
-	#arguments = parser.parse_args(sys.argv[1:]) 
-	#print arguments.file
-	#print arguments.trash_location
-	#if arguments.command = 'cleantrash':
-
+	arguments = parser.parse_args(sys.argv[1:]) 
+	if arguments.type_of_command == 'trash':
+		delete_in_trash(arguments.file, file_location, trash_location)
+	elif arguments.type_of_command == 'recover_from_trash':
+		recover_from_trash(arguments.file)
 	
 
 main()

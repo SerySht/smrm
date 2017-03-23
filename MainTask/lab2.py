25829a4be6c9e7d4b7b85d9cfae033be5731ab0f
Sergey <sery.sht@gmail.com>
2017-03-24 01:57:33 +0300

    removing file from list.txt

--- MainTask/lab2.py
+++ MainTask/lab2.py
@@ -13,18 +13,28 @@ def show_trash(trash_location):
 	print os.listdir(trash_location) 
 
 def delete_file(filename):
-	shutil.remove(filename)
+	os.remove(filename)
 
 def delete_directory():
 	shutil.rmtree(directory_location)
 
 def recover_from_trash(filename):
-	f = open('Trash/list.txt', 'r')
+	file_list = []
+	f = open('Trash/list.txt', 'r+')
 	for line in f:
-		line = line.split()   #could be better way	
+		file_list.append(line)
+	f.close()
+	
+
+
+	for i in xrange(len(file_list)):		
+		line = file_list[i].split()   #could be better way	
 		if filename == line[0]:			
 			shutil.move("Trash/"+filename, line[1])
-			line = ""   #how delete line, m?
+			file_list.pop(i)
+			#
+			f = open('Trash/list.txt', 'w')
+			f.writelines(file_list)
 			f.close()
 			return
 	print "There is no such file!"	
@@ -32,10 +42,10 @@ def recover_from_trash(filename):
 
 
 
-def delete_in_trash(filename, file_location, trash_location):
+def delete_to_trash(filename, file_location, trash_location):
 	shutil.move(filename, trash_location)
 	f = open('Trash/list.txt', 'a')
-	f.write(filename + ' ' + file_location)
+	f.write(filename + ' ' + file_location + '\n')
 	f.close()
 
 
@@ -52,13 +62,15 @@ def main():
 
 	
 	if arguments.type_of_command == 'trash':
-		delete_in_trash(arguments.file, file_location, trash_location)
+		delete_to_trash(arguments.file, file_location, trash_location)
 	elif arguments.type_of_command == 'recover_from_trash':
 		recover_from_trash(arguments.file)
 	elif arguments.type_of_command == 'clean_trash':
 		clean_trash(trash_location)
 	elif arguments.type_of_command == 'show_trash':
 		show_trash(trash_location)
+	elif arguments.type_of_command == 'without_recover':
+		delete_file(arguments.file)
 	else: print "There is no such command!"
 
 main()

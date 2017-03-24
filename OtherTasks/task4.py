be70638ccaaf997595f1e6f49c63fd8d87ecb72b
Sergey <sery.sht@gmail.com>
2017-03-24 13:01:33 +0300

    fixed task4 (fib)

--- OtherTasks/task4.py
+++ OtherTasks/task4.py
@@ -1,17 +1,22 @@
-def fib(filename):
-    f = open(filename, "r")
-    text = f.readline()
+def fib(n):
     a = 1
     b = 1
     print a, "\n", b
-    for i in range(10):
-        c = b
-        b += a
-        a = c
-        print b
+    if n > 2 :
+        for i in range(n-2):
+            c = b
+            b += a
+            a = c
+            yield b
+
+def main():
+    n = 10
+    for i in fib(n):
+        print i
 
 if __name__ == "__main__":
-    fib()
+    main()
+    
 
 
 

3b0fa8c2e9bc760eeab9211e01db892e825f4591
Sergey S <sery.sht@gmail.com>
2017-05-06 00:56:06 +0300

    pep

--- smrm/config.py
+++ smrm/config.py
@@ -3,42 +3,42 @@ import json
 
 
 def load(directory = '/home/sergey/labs/lab2/smrm/smrm.conf'):
-	conf = ConfigParser.RawConfigParser()            
-	conf.read(directory)
-	
-	if conf.has_option("main", "trash_location"):
-		trash_location = conf.get("main", "trash_location")
- 	else: 
- 		trash_location = ''
+    conf = ConfigParser.RawConfigParser()            
+    conf.read(directory)
+    
+    if conf.has_option("main", "trash_location"):
+        trash_location = conf.get("main", "trash_location")
+    else: 
+        trash_location = ''
 
- 	if conf.has_option("main", "log_location"):
-		log_location = conf.get("main", "log_location")
-	else:
-		log_location = ''
-	
-	if conf.has_option("main", "recover_conflict"):
-		recover_conflict = conf.get("main", "recover_conflict")
-	else: 
-		recover_conflict = 'not_replace'	
-	
-	if conf.has_option("politics", "storage_time"):
-		storage_time = conf.get("politics", "storage_time") 
-	else: 
-		storage_time = ''
+    if conf.has_option("main", "log_location"):
+        log_location = conf.get("main", "log_location")
+    else:
+        log_location = ''
+    
+    if conf.has_option("main", "recover_conflict"):
+        recover_conflict = conf.get("main", "recover_conflict")
+    else: 
+        recover_conflict = 'not_replace'    
+    
+    if conf.has_option("politics", "storage_time"):
+        storage_time = conf.get("politics", "storage_time") 
+    else: 
+        storage_time = ''
 
-	if conf.has_option("politics", "trash_maximum_size"):
-		trash_maximum_size = conf.get("politics", "trash_maximum_size")
-	else:
-		trash_maximum_size = ''
+    if conf.has_option("politics", "trash_maximum_size"):
+        trash_maximum_size = conf.get("politics", "trash_maximum_size")
+    else:
+        trash_maximum_size = ''
 
 
-	json_config = {'trash_location':trash_location,
-					'log_location':log_location, 
-					'recover_conflict':recover_conflict, 
-					'storage_time':storage_time,
-					'trash_maximum_size':trash_maximum_size}
+    json_config = {'trash_location':trash_location,
+                    'log_location':log_location, 
+                    'recover_conflict':recover_conflict, 
+                    'storage_time':storage_time,
+                    'trash_maximum_size':trash_maximum_size}
 
-	f = open(directory[:-4]+'json', 'a+')                
-	f.write(json.dumps(json_config))
+    f = open(directory[:-4]+'json', 'a+')                
+    f.write(json.dumps(json_config))
 
-	return json_config			
+    return json_config          
--- smrm/test.py
+++ smrm/test.py
@@ -7,11 +7,11 @@ import smrm
 
 class TestTrash(unittest.TestCase):
 
-	def test_add_slash(self):
-		os.mknod("bekmek1")
-		smrm.main()
+    def test_add_slash(self):
+        os.mknod("bekmek1")
+        smrm.main()
 
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
